533c954da5edecf89b2ca5ea21fa63e6554d1ab1
Joimar <joimarbgf@gmail.com>
2015-12-22 02:36:30 +0000

    Update Lexico.java

--- AnalizadorLexico/src/analizadorlexico/Lexico.java
+++ AnalizadorLexico/src/analizadorlexico/Lexico.java
@@ -31,6 +31,23 @@ public class Lexico {
         
     }
     
-    
+     public boolean isIdentificador(String palavras)
+    {
+       // int i = indice;
+       boolean identificador = false;
+       String aux ="";
+       int iaux=0; 
+        for(int i= indice; palavras.charAt(i)!=' '; i++)
+        {
+            
+            if(palavras.charAt(i+1) == ' ') return true;
+            else if(palavras.charAt(i+1)>= 48 && palavras.charAt(i+1)<= 57) identificador = true;
+            else if(palavras.charAt(i+1)>= 65 && palavras.charAt(i+1)<= 90) identificador = true;
+            else if( (palavras.charAt(i+1)>= 97 && palavras.charAt(i+1)<= 122) || (palavras.charAt(i+1) == '_') ) identificador = true;
+            else identificador = false;
+           // aux.= palavras.charAt(i); //nao lembro como igualar caracterer por caractere 
+            iaux++;
+        
+        }
     
 }
